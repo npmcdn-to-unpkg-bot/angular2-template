fe15952ac87bccd23dc4ab488525a6398fd8b98a
Svetozar Toskov <s.toskov@abv.bg>
2016-02-08 19:10:59 +0200

    move angular outside mode_modules

--- angular2-template/app/app.component.js
+++ angular2-template/app/app.component.js
@@ -23,7 +23,7 @@ System.register(['angular2/core'], function(exports_1) {
                     core_1.Component({
                         selector: 'my-app',
                         template: '<h1>My First Angular 2 App</h1>'
-                    }), 
+                    }),
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
